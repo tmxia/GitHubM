b1a422e8223e4691b7b00e300fa7390f643665ef
tmxia <259732@qq.com>
2026-09-26 04:26:07 +0800

    fix

--- src/pages/ActionsPage.tsx
+++ src/pages/ActionsPage.tsx
@@ -101,7 +101,7 @@ function AnsiLine({ raw }: { raw: string }) {
   if (m) {
     return (
       <span>
-        <span className="text-[#6e7681] select-none mr-2 text-[10px]">{m[1].replace('T', ' ').replace('Z', '')}</span>
+        <span className="text-[#e6edf3] select-none mr-2 text-[10px]">{m[1].replace('T', ' ').replace('Z', '')}</span>
         <span className="text-[#e6edf3]">{m[2]}</span>
       </span>
     );
